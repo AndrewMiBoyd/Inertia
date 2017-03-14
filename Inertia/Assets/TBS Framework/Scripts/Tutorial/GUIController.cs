24cdfa0bacb340683d7308660a2aa53f0afcfe63
Blake Dixon <blaketdixon11@gmail.com>
2017-03-13 19:57:44 -0700

    movement awesomeness

---
+++ Inertia/Assets/TBS Framework/Scripts/Tutorial/GUIController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GUIController : MonoBehaviour
+{
+    public CellGrid CellGrid;
+	
+    void Start()
+    {
+        Debug.Log("Press 'n' to end turn");
+    }
+
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            CellGrid.EndTurn();//User ends his turn by pressing "n" on keyboard.
+			Debug.Log("A Turn Occured");
+        }
+	}
+}

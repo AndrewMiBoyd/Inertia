5609c423cd1f43e145050b92c625855f2a7c7c16
Buzz Tilford <tilford20@gmail.com>
2017-03-19 00:24:48 -0700

    made the function generic for use in all positions

--- arcListGenerator.js
+++ arcListGenerator.js
@@ -39,10 +39,11 @@ var getNextCells = function(list, position)
     });   
 
 }
-var generateCellList = function(position)
+var generateCellList = function(position, rotation)
 {
-    var list = {};
-    list.push = function(item)
+    var list = [];
+    set = {};
+    set.push = function(item)
     {
         this[JSON.stringify(item)] = true;
     }
@@ -57,8 +58,50 @@ var generateCellList = function(position)
         j: position.j,
         k: position.k})
 
-
-    console.log(list);
+    for (var x = 0; x < list.length; x++)
+    {
+        switch(rotation)
+        {   
+            case 1:
+                var rotated = {};
+                rotated.j = list[x].i;
+                rotated.k = list[x].j;
+                rotated.i = -list[x].k;
+                set.push(rotated);
+                break;
+            case 2:
+                var rotated = {};
+                rotated.j = -list[x].k
+                rotated.k = list[x].i;
+                rotated.i = - list[x].j;
+                set.push(rotated);
+                break;
+            case 3:
+                var rotated = {};
+                rotated.j = -list[x].k
+                rotated.k = - list[x].j
+                rotated.i = - list[x].i;
+                set.push(rotated);
+                break;
+            case 4:
+                var rotated = {};
+                rotated.j = - list[x].i;
+                rotated.k = - list[x].j;
+                rotated.i =  list[x].k
+                set.push(rotated);
+                break;
+            case 5:
+                var rotated = {};
+                rotated.j = list[x].k;
+                rotated.k = - list[x].i;
+                rotated.i = list[x].j
+                set.push(rotated);
+                break;
+            default:
+                set.push(list[x])
+        } 
+    }   
+    console.log(set);
 }
-generateCellList({i:0, j:0, k:0});
+generateCellList({i:0, j:0, k:0}, 1);
 

85b2f962e84d638a472a8519d13d3d962a25419c
Buzz Tilford <tilford20@gmail.com>
2017-03-22 16:36:21 -0700

    implemented the priotity calculations when applying manuevers

--- applyManuever.cs
+++ applyManuever.cs
@@ -1,60 +1,88 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class applyManuever: MonoBehaviour {
+    public int T=0;
+    public int A=0;
 
-
-public String createOrderOfAction(int rotationalInertia, int acceleration)
+    void Start()
+    {
+    createOrderOfAction(T, A);
+    }
+public string createOrderOfAction(int rotationalInertia, int acceleration)
 {
-    List<String> actionOrder = new List<String>();
+    List<string> actionOrder = new List<string>(0);
 
+    actionOrder.Add("");
+    actionOrder.Add("");
+    actionOrder.Add("");
+    actionOrder.Add("");
+    
     int i = actionOrder.Count/2;
 
     int turnsHalf = rotationalInertia/2;
-    int accelerationHalf = accerlertaion/2;
+    int accelerationHalf = acceleration/2;
 
     int leftOverRotation = rotationalInertia % 2;
     int leftOverAcceleration = acceleration % 2;
 
-    int commandsInList = 0;
+    //int commandsInList = 0;
 
     while(turnsHalf > 1 && accelerationHalf > 1)
     {
+        Debug.Log("Hey, made it into the loo00p");
         if (turnsHalf > accelerationHalf)
         {
-            actionOrder.insert(i+1, "T");
-            actionOrder.insert(i+1, "T");
+            actionOrder.Insert(i+1, "T");
+            actionOrder.Insert(i+1, "T");
             turnsHalf -= 2;
             i = actionOrder.Count/2;
         }
         else if (turnsHalf < accelerationHalf)
         {
-            actionOrder.insert(i+1, "A");
-            actionOrder.insert(i+1, "A");
+            actionOrder.Insert(i+1, "A");
+            actionOrder.Insert(i+1, "A");
             accelerationHalf -=2;
             i = actionOrder.Count/2;
         }
         else if (turnsHalf == accelerationHalf)
         {
-            actionOrder.insert (i+1, "A");
-            actionOrder.insert(i+1, "T");
+            actionOrder.Insert(i+1, "T");
+            actionOrder.Insert (i+1, "A");
             turnsHalf -=1;
             accelerationHalf --;
         }
     }
-
-    while (accelerationHalf > 0)
-    { actionOrder.insert(i+1, "A");
-        accelerationHalf--;
+    
+    while (accelerationHalf + turnsHalf > 0)
+    { 
+    
+        if  (turnsHalf > 0)
+        { 
+            actionOrder.Insert(i, "T");
+            turnsHalf--;
+        }
+        if (accelerationHalf>0)
+        {
+            actionOrder.Insert(i, "A");
+            accelerationHalf--;
+        }
     }
-    while (turnsHalf > 0)
-    { actionOrder.insert(i+1, "T");
-        TurnsHalf--;
+    actionOrder.Reverse();
+    List <string> tempList = new List<string>(actionOrder);
+    tempList.Reverse();
+    actionOrder.AddRange( tempList); 
+    i = actionOrder.Count/2;
+    while (leftOverRotation > 0)
+    { actionOrder.Insert(i+1, "T");
+        leftOverRotation--;
     }
-    actionOrder.Concat(actionOrder;)
-    while (accelerationHalf > 0)
-    { actionOrder.insert(i+1, "A");
-        accelerationHalf--;
+    while (leftOverAcceleration > 0)
+    { actionOrder.Insert(i+1, "A");
+      leftOverAcceleration--;
     }
-    while (turnsHalf > 0)
-    { actionOrder.insert(i+1, "T");
-        turnsHalf--;
-    }
+    string actions = string.Join(string.Empty, actionOrder.ToArray());
+    Debug.Log(actions);
+    return actions;//returnValue;
+}
 
 }

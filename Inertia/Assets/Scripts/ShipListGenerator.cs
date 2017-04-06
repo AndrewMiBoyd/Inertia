369f0e5fcc7a4e9997ddeea784b7bd2164f9fb2b
Blake Dixon <blaketdixon11@gmail.com>
2017-04-05 17:55:38 -0700

    UI end turn button working, minor UI appearance changes

---
+++ Inertia/Assets/Scripts/ShipListGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipListGenerator : MonoBehaviour {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
--- Inertia/Assets/TBS Framework/Scripts/Tutorial/GUIController.cs
+++ Inertia/Assets/TBS Framework/Scripts/Tutorial/GUIController.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.Events;
+//using UnityEngine.UI;
 
 public class GUIController : MonoBehaviour
 {
-    public CellGrid CellGrid;
-	
+    //public CellGrid CellGrid;
+//	public Button button;
+/*
     void Start()
     {
         Debug.Log("Press 'n' to end turn");
@@ -11,10 +15,15 @@ public class GUIController : MonoBehaviour
 
 	void Update ()
     {
-        if(Input.GetKeyDown(KeyCode.N))
+		if(false)
         {
             CellGrid.EndTurn();//User ends his turn by pressing "n" on keyboard.
 			Debug.Log("A Turn Occured");
         }
+	}
+*/
+	public void TaskOnClick(CellGrid CellGrid) {
+		CellGrid.EndTurn();
+		Debug.Log("A Turn Occured");
 	}
 }
